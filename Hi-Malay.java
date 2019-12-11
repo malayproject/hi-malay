a41779787eef0a2bb2d6f1375635735958c02622
malayproject <55144991+malayproject@users.noreply.github.com>
2019-12-11 10:47:25 +0530

    class hi-malay added
    first java file added

---
+++ Hi-Malay.java
@@ -0,0 +1,18 @@
+package com.malay.Hi-Malay
+
+class Hi-Malay  {
+
+  private String hi;
+  private String name;
+
+  public Hi-Malay (){}
+
+  public Hi-Malay (String name) {
+    this.name = name;
+  }
+
+  public void sayHi() {
+    System.out.println("Hi-Malay");
+  }
+
+}
